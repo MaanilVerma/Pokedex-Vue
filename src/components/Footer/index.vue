6e7fd443f73d183cadcc1e8f6f546b3cbc3aad7a
Maanil Verma <vermaMaanil97@gmail.com>
2024-05-14 19:32:34 +0530

    feat: add footer with styles

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div className="main-container">
+    <footer class="footer">
+      <div class="footer-text">
+        <span class="credits">
+          Developed By
+          <a href="https://maanilverma.netlify.app" target="_blank">
+            Maanil Verma
+          </a>
+        </span>
+        <span>Data taken from API - pokeapi.co</span>
+      </div>
+      <SocialMedia></SocialMedia>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SocialMedia from "../SocialMedia/index.vue";
+import "./index.scss";
+</script>
