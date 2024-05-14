3292426a4b641d6d28d969346e7eaf4e8838639b
Maanil Verma <vermaMaanil97@gmail.com>
2024-05-14 19:30:25 +0530

    feat: add scroll indicator

---
+++ src/components/ScrolldownIndicator/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="scroll-down-container"
+    :style="{ display: isVisible ? 'flex' : 'none' }"
+  >
+    <div class="chevron" />
+    <div class="chevron" />
+    <div class="chevron" />
+    <span class="scroll-down-text">Scroll down</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue";
+const isVisible = ref<boolean>(true);
+
+const handleScroll = () => {
+  isVisible.value = false;
+  window.removeEventListener("scroll", handleScroll);
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
