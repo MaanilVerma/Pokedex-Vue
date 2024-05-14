4629765f324e8a4e051c78773986908219e626d0
Maanil Verma <vermaMaanil97@gmail.com>
2024-05-14 19:31:57 +0530

    feat: add  general loader with styles

---
+++ src/components/Loader/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="loader-container">
+    <div class="loader">
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+</script>
