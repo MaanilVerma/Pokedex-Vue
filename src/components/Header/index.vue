4cb36149031e89101842a609c3e6a8d99bc9cb08
Maanil Verma <vermaMaanil97@gmail.com>
2024-05-14 19:32:20 +0530

    feat: add header with styles

---
+++ src/components/Header/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import "./index.scss";
+import PokemonLogo from "../../assets/images/PokemonLogo.vue";
+import SocialMedia from "../SocialMedia/index.vue";
+</script>
+
+<template>
+  <div className="main-container">
+    <header class="header">
+      <PokemonLogo />
+      <SocialMedia />
+    </header>
+  </div>
+</template>
