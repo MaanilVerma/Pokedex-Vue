c36e2aeb2d054e9decebd3c53dbaf8c786ec373a
Maanil Verma <vermaMaanil97@gmail.com>
2024-05-14 19:32:46 +0530

    feat: add error message with styles

---
+++ src/components/ErrorMessage/index.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import "./index.scss";
+
+export default {
+  name: "ErrorMessage",
+};
+</script>
+
+<template>
+  <div class="error-message-container">
+    <div class="error-message">
+      <img
+        alt="Pikachu"
+        class="error-image"
+        src="@/assets/images/img-pikachu-sad-min.png"
+        width="32"
+        height="32"
+      />
+      <span>Oops, Pokemon Not Found!</span>
+    </div>
+  </div>
+</template>
